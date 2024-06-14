8bd4378bceb1ef3b01117a7d79accc38edfef326
sowmik-sec <sowmik.sec@gmail.com>
2024-06-14 10:19:10 +0600

    let const

---
+++ var-let-const.js
@@ -0,0 +1,19 @@
+const money = 25;
+const rich = money + 25;
+console.log(rich);
+let count = 0;
+count += 10;
+console.log(count);
+
+const numbers = [23, 4, 23, 12, 56];
+numbers[3] = 32;
+console.log(numbers);
+numbers.push(9, 10, 100);
+const student = {
+  name: "moyna pakhi",
+  class: 12,
+};
+
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
